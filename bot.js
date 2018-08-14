457ce46ad8743674cb7da3e07564eee438ed98fe
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-08-14 12:48:50 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+
+**سلام عليكم يا قلبي 
+جبت لكم افضل سيرفر سوالف 
+تنورنا يحلو
+احبك
+this is chat server
+join to server you will be happy
+love you
+
+                               [ https://discord.gg/uUraTA3
+] 
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم يا قلبي 
+جبت لكم افضل سيرفر سوالف 
+تنورنا يحلو
+احبك
+this is chat server
+join to server you will be happy
+love you
+
+
+                               [ https://discord.gg/uUraTA3
+ ] 
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+client.login('');
